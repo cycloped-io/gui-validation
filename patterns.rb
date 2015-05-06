ab8b7ce3fe6643e97fa2e97a1bef7288957b7afc
Krzysztof Wróbel <djstrong@gmail.com>
2015-05-06 11:05:27 +0200

    ptterns validation

--- patterns.rb
+++ patterns.rb
@@ -1,10 +1,15 @@
+require 'bundler/setup'
 require 'sinatra'
 require 'csv'
 require 'slop'
+require 'cycr'
+require 'mapping'
 
-input_path = ENV['INPUT_PATH'] || 'data/patterns/patterns.csv'
+input_path = ENV['INPUT_PATH'] || 'data/patterns/with_examples_and_maping.csv'
 output_path = ENV['OUTPUT_PATH'] || input_path+'.out'
 
+cyc = Cyc::Client.new(cache: true, host: 'localhost', port: 3601)
+name_service = Mapping::Service::CycNameService.new(cyc)
 
 selected = []
 
@@ -32,19 +37,41 @@ end
 get '/:id' do
   counter = params[:id].to_i
   if counter<selected.size then
-    pattern, support, *categories = selected[counter]
+    pattern, cyc_id, cyc_name, support, *categories = selected[counter]
     #wiki_url = 'http://en.wikipedia.org/wiki/'+wiki
     #cyc_url = 'http://sw.opencyc.org/concept/'+cyc
-    erb :patterns, :locals => {:pattern => pattern, :support => support.to_i, :categories => categories, :counter => counter}
+
+    if !cyc_name.nil?
+      cyc_url = 'http://sw.opencyc.org/concept/'+cyc_id
+    else
+      cyc_url = 'http://sw.opencyc.org/'
+    end
+
+    erb :patterns, :locals => {:pattern => pattern, :support => support.to_i, :cyc_url => cyc_url, :cyc => cyc_name, :categories => categories, :counter => counter}
   else
     erb :finish
   end
 end
 
 post '/:action/:id' do
-  if params[:id] =~ /^\d+$/ && %w{v i u}.include?(params[:action])
+  if params[:id] =~ /^\d+$/ && %w{v set}.include?(params[:action])
     counter = params[:id].to_i
-    output << [params[:action]]+selected[counter]
+    pattern, cyc_id, cyc_name, support, *categories = selected[counter]
+
+    if params[:action] == 'set'
+      cyc_name = params[:cyc_name]
+      p cyc_name
+      term = name_service.find_by_term_name(cyc_name)
+      p term
+      if !term.nil?
+        cyc_id = cyc.compact_hl_external_id_string(term)
+      else
+        cyc_id = ''
+      end
+    end
+
+
+    output << [pattern, cyc_id, cyc_name]
     output.flush
     counter += 1
     redirect to("/#{counter}")
